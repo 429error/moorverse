978ae86f4ac296653174fbdccdf460f5689ad62c
Sneha <144522786+429error@users.noreply.github.com>
2025-07-06 16:07:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@
     ],
     songs: [
      "https://www.youtube.com/embed/e20EFLNwHuQ",  
-     "https://www.youtube.com/embed/6NhJMhbFTNcy",  
      "https://www.youtube.com/embed/2OEL4P1Rz04",  // Lofi happy beats
      "https://www.youtube.com/embed/DWcJFNfaw9c",
      "https://www.youtube.com/embed/CaVnYhhTYsE",
